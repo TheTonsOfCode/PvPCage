5e074d0320fd8a767acf4b8ec6d917e0a19db22e
TheTonsOfCode <pawe9145@gmail.com>
2015-10-24 23:06:42 +0200

    CageCMD

--- src/com/noname/pvpcage/commands/CageCmd.java
+++ src/com/noname/pvpcage/commands/CageCmd.java
@@ -14,13 +14,19 @@ public class CageCmd extends Command {
                 @Override
                 protected void execute() {
                     new WalledCage().onCreateBattle(player.getLocation());
+                    send("&aStworzono klatke!");
                 }  
         });
         addSubCommand(new SubCommand("world", "tworzy cage world'a", "w") {
                 @Override
                 protected void execute() {
-                    CageBuilder.createCageWorld();
+                    if(CageBuilder.getCageWorld() == null) {
+                        CageBuilder.createCageWorld();
+                        send("&6Stworzono &7Cage World!");
+                        return;
+                    }
                     CageBuilder.teleportToCageWorld(player);
+                    send("&aPrzteleportowano do Cage World'u!");
                 }  
         });
     }
