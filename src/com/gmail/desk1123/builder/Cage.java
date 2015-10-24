1cb5e7567cb28aade564cf96dd93c362e0fcdb41
Michal Cieslinski <desk1123@gmail.com>
2015-10-24 19:17:23 +0200

    new method

--- src/com/gmail/desk1123/builder/Cage.java
+++ src/com/gmail/desk1123/builder/Cage.java
@@ -1,21 +1,23 @@
 package com.gmail.desk1123.builder;
 
+import com.gmail.desk1123.objets.User;
 import java.util.ArrayList;
 import java.util.List;
 import org.bukkit.Location;
 
-
 public abstract class Cage {
+
     public abstract void onCreateBattle(Location loc);
 
+    public abstract void onEndBattle(User winner);
     protected List<Location> blocks = new ArrayList<>();
-    
+
     private CageType cageType;
 
     public Cage(CageType cageType) {
         this.cageType = cageType;
-    }      
-    
+    }
+
     public CageType getCageType() {
         return cageType;
     }
