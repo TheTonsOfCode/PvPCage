1c7b76626481f83e87318ae086f1a7562de13b15
Michal Cieslinski <desk1123@gmail.com>
2015-10-28 15:50:53 +0100

    zapis do pliku graczy oraz FIleManager

--- src/com/noname/pvpcage/objects/User.java
+++ src/com/noname/pvpcage/objects/User.java
@@ -1,6 +1,22 @@
 package com.noname.pvpcage.objects;
 
+import com.noname.pvpcage.PvPCage;
+import com.noname.pvpcage.Test.Item;
+import com.noname.pvpcage.Test.ItemManager;
+import com.noname.pvpcage.managers.FileManager;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
 /**
@@ -16,7 +32,11 @@ public class User {
     private int escapeDuel;
     private int points;
     private long lastSave;
-    //make invite
+    private String team;
+    private long lastSeen;// to check if somethink....
+    private List<String> victims = new ArrayList<>();
+    private Player player;
+    private List<Item> selectedItem = new ArrayList<>();
 
     public User(UUID uuid) {
         winDuel = 0;
@@ -25,15 +45,58 @@ public class User {
         escapeDuel = 0;
         lastSave = System.currentTimeMillis();
         points = 0;
+        team = null;
+        player = null;
+        victims.clear();
+        selectedItem = ItemManager.getItems();
     }
 
-    public static User fromPlayer(Player player) {
-        if (player != null) {
-            User user = new User(player.getUniqueId());
-            user.setName(player.getName());
-            return user;
+    public List<Item> getSelectedItems() {
+        return selectedItem;
+    }
+
+    public void clearSelctedItems() {
+        selectedItem = ItemManager.getItems();
+    }
+
+    public List<String> getVictims() {
+        return victims;
+    }
+
+    public void addVictim(String uuid) {
+        if (!this.victims.contains(uuid)) {
+            this.victims.add(uuid);
         }
-        return null;
+    }
+
+    public void removeVictim(String uuid) {
+        if (this.victims.contains(uuid)) {
+            this.victims.remove(uuid);
+        }
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public long getLastSeen() {
+        return lastSeen;
+    }
+
+    public void setLastSeen(long lastSeen) {
+        this.lastSeen = lastSeen;
     }
 
     public String getName() {
@@ -92,12 +155,141 @@ public class User {
         this.lastSave = lastSave;
     }
 
-    public void loadFromMySQL() {
+    private String victimsToString() {
+        String s = null;
+        for (String name : victims) {
+            s = name + ",";
+        }
+        return s;
+    }
 
+    private void stringToVictims(String s) {
+        String[] name = s.split(",");
+        for (String name1 : name) {
+            victims.add(name1);
+        }
+    }
+
+    public void loadFromMySQL() {
+        Connection conn = PvPCage.getMySQL().getConnection();
+        if (conn == null) {
+            try {
+                conn = PvPCage.getMySQL().openConnection();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement("SELECT * FROM `UserDataPvPCage` WHERE `uuid`=?");
+            st.setString(1, uuid.toString());
+            rs = st.executeQuery();
+            if (rs.next()) {
+                name = rs.getString("name");
+                team = rs.getString("team");
+                loseDuel = rs.getInt("loseduel");
+                winDuel = rs.getInt("winduel");
+                escapeDuel = rs.getInt("escapeduel");
+                points = rs.getInt("points");
+                stringToVictims(rs.getString("victims"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        PvPCage.getMySQL().closeResources(rs, st);
     }
 
     public void saveToMySQL() {
+        Connection conn = PvPCage.getMySQL().getConnection();
+        if (conn == null) {
+            try {
+                conn = PvPCage.getMySQL().openConnection();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
+        PreparedStatement st = null;
+        StringBuilder query = new StringBuilder();
+        query.append("INSERT INTO `UserDataPvPCage` (`uuid`, `name`, `team`, `loseduel`, `winduel`,`escapeduel`,`points`, `victims`)")
+                .append("VALUES (?,?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE ")
+                .append(" `uuid`=VALUES(`uuid`), `name`=VALUES(`name`), `team`=VALUES(`team`), ")
+                .append("`loseduel`=VALUES(`loseduel`), `winduel`=VALUES(`winduel`),")
+                .append("`escapeduel`=VALUES(`escapeduel`), `points`=VALUES(`points`),")
+                .append("`victims`=VALUES(`victims`)");
+        try {
+            st = conn.prepareStatement(query.toString());
+            st.setString(1, uuid.toString());
+            st.setString(2, name);
+            st.setString(3, team);
+            st.setInt(4, loseDuel);
+            st.setInt(5, winDuel);
+            st.setInt(6, escapeDuel);
+            st.setInt(7, points);
+            st.setString(8, victimsToString());
+            st.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        PvPCage.getMySQL().closeResources(null, st);
+    }
+
+    /*
+     private String name;
+     private UUID uuid;
+     private int winDuel;
+     private int loseDuel;
+     private int escapeDuel;
+     private int points;
+     private long lastSave;
+     private String team;
+     private long lastSeen;// to check if somethink....
+     private List<UUID> victims = new ArrayList<>();
+     private Player player;
+     private List<Item> selectedItem = new ArrayList<>();
+     */
+    public void saveToFile() {
+        File f = new File(FileManager.getUserData(), "");
+        if (!f.exists()) {
+            try {
+                f.createNewFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        FileConfiguration yml = YamlConfiguration.loadConfiguration(f);
+        yml.set("Name", name);
+        yml.set("Uuid", uuid);
+        yml.set("WinDuel", winDuel);
+        yml.set("LoseDuel", loseDuel);
+        yml.set("EscapeDuel", escapeDuel);
+        yml.set("Points", points);
+        yml.set("LastSeen", lastSeen);
+        yml.set("Team", team);
+        yml.set("Victims", victims);
+        try {
+            yml.save(f);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void loadFromFile() {
+        File f = new File(FileManager.getUserData(), "");
+        if (!f.exists()) {
+            return;
+        }
+        FileConfiguration yml = YamlConfiguration.loadConfiguration(f);
+        name = yml.getString("Name");
+        uuid = UUID.fromString(yml.getString("Uuid"));
+        winDuel = yml.getInt("WinDuel");
+        loseDuel = yml.getInt("LoseDuel");
+        escapeDuel = yml.getInt("EscapeDuel");
+        points = yml.getInt("Points");
+        lastSeen = yml.getLong("LastSeen");
+        team = yml.getString("team");
+        victims = yml.getStringList("Victims");
     }
 
 }
--- src/com/noname/pvpcage/utilities/Configuration.java
+++ src/com/noname/pvpcage/utilities/Configuration.java
@@ -1,25 +1,28 @@
 package com.noname.pvpcage.utilities;
 
 import com.noname.pvpcage.PvPCage;
+import com.noname.pvpcage.managers.FileManager;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 public class Configuration {
 
     public static FileConfiguration config;
-
+    public static FileConfiguration cageConfig = YamlConfiguration.loadConfiguration(FileManager.getCageConfig());
+    public static FileConfiguration partyConfig = YamlConfiguration.loadConfiguration(FileManager.getPartyConfig());
+    
     public static Material CAGE_WALL_MATERIAL;
     public static Material CAGE_FLOOR_MATERIAL;
-    
+
     public static int USER_START_RANKING;
-    
-    
+
     public static int PARTY_TAG_LENTH;
     public static int PARTY_NAME_LENTH;
     public static int PARTY_MAX_MEMBERS;
 
     public static int LOGGER_LEVEL;
-    
+
     public static String HOST;
     public static String PASSWORD;
     public static String DATABASE;
@@ -35,9 +38,9 @@ public class Configuration {
         DATABASE = config.getString("MySQL.Database");
         USER = config.getString("MySQL.User");
         PORT = config.getInt("MySQL.Port");
-        
-        CAGE_WALL_MATERIAL = Material.getMaterial(config.getString("Cage.Wall_Material"));
-        CAGE_FLOOR_MATERIAL = Material.getMaterial(config.getString("Cage.Floor_Material"));
+
+        CAGE_WALL_MATERIAL = Material.getMaterial(cageConfig.getString("WalledCage.Wall_Material"));
+        CAGE_FLOOR_MATERIAL = Material.getMaterial(cageConfig.getString("WalledCage.Floor_Material"));
         PvPCage.getInstance().saveConfig();
     }
 
