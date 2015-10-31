830b965f7f523488ad431906a3461e25bbcb7882
TheTonsOfCode <pawe9145@gmail.com>
2015-10-31 15:29:20 +0100

    table ?

--- src/com/noname/pvpcage/utilities/data/Table.java
+++ src/com/noname/pvpcage/utilities/data/Table.java
@@ -1,12 +1,33 @@
 package com.noname.pvpcage.utilities.data;
 
 public enum Table {
-    DEATHS,
-    TEAMS,
-    USERS;
+    DEATHS() {
+        @Override
+        public String getCreateTableQuery() {
+            return "Create Table if not exists " + getTableName();//i dalej co tam ma byc
+        }      
+    },
+    TEAMS {
+        @Override
+        public String getCreateTableQuery() {
+            return "Create Table if not exists " + getTableName();//i dalej co tam ma byc
+        }      
+    },
+    USERS {
+        @Override
+        public String getCreateTableQuery() {
+            return "Create Table if not exists " + getTableName();//i dalej co tam ma byc
+        }      
+    };
 
+    private Table() {
+        
+    }
     
+    public abstract String getCreateTableQuery();
     
-    
+    public String getTableName() {
+        return "PVP" + name();
+    }
     
 }
