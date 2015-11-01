66671b42277cfa2318a188266c9781bcd8888bea
Dekros987 <desk1123@gmail.com>
2015-11-01 20:22:18 +0100

    ImAdd!

--- Resources/createTeams.sql
+++ Resources/createTeams.sql
@@ -1,1 +1,10 @@
-
+CREATE TABLE IF NOT EXISTS `PartyDataPvPCage` (
+    `id` INT(6) NOT NULL AUTO_INCREMENT,
+    `tag` VARCHAR(6) NOT NULL UNIQUE,
+    `name` VARCHAR(16) NOT NULL UNIQUE,
+    `leader` VARCHAR(36),
+    `mod` VARCHAR(36),
+    `invited` VARCHAR(100),
+    PRIMARY KEY (`id`),
+    KEY (`tag`, `name`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
